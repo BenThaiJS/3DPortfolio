128b6f5e804336da8219807d7a79dffe84e9c225
Benjamin Thai <itsbenthai@gmail.com>
2023-12-04 22:30:33 -0800

    add alert message and 3d fox

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,6 @@ const Navbar = () => {
         </NavLink>
         <nav className="flex text-lg gap-7 font-medium">
             <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>About</NavLink>
-        </nav>
-        <nav className="flex text-lg gap-7 font-medium">
             <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>Projects</NavLink>
         </nav>
     </header>
